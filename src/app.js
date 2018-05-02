489f7436d6289cec52c0d7f57c4ae50d813c6510
Robert Claypool <robert.g.claypool+git@gmail.com>
2018-05-02 17:34:47 -0500

    Fix an ESLint error

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
-import './scss/app.scss';
 // If .bablerc enables `useBuiltIns: 'entry'`, then Babel will replace the
 // following `import "@babel/polyfill"` with requires for `@babel/polyfill`
 // based on environment, see https://github.com/babel/babel/tree/master/packages/babel-preset-env
 import '@babel/polyfill';
+import './scss/app.scss';
 
 // This function is to demonstrate Babel.
 // Search for "World" in the dist/ output bundle to see what has changed.
